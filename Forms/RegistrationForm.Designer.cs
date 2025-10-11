69a4c210054f334ed7b96e7e167e7e888fcd8e21
rapp-afk <@rafafathurrohman42@gmail.com>
2025-10-11 13:03:35 +0700

    Product

---
+++ Forms/RegistrationForm.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Nusantara.Forms
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textQuest1 = new TextBox();
+            textEmail = new TextBox();
+            textCardId = new TextBox();
+            textFullname = new TextBox();
+            textQuest2 = new TextBox();
+            textReferenceId = new TextBox();
+            textPhone = new TextBox();
+            textUsername = new TextBox();
+            textPhoneAlt = new TextBox();
+            textPassword = new TextBox();
+            lblFullName = new Label();
+            lblCardID = new Label();
+            lblEmail = new Label();
+            lblColor = new Label();
+            lblUsername = new Label();
+            lblPhone = new Label();
+            lblRefID = new Label();
+            label8 = new Label();
+            lblArtist = new Label();
+            lblPassword = new Label();
+            lblPhoneAlt = new Label();
+            buttonSummit = new Button();
+            buttonBack = new Button();
+            labelError = new Label();
+            lblAddress = new Label();
+            textAddress = new TextBox();
+            SuspendLayout();
+            // 
+            // textQuest1
+            // 
+            textQuest1.Location = new Point(51, 323);
+            textQuest1.Name = "textQuest1";
+            textQuest1.Size = new Size(434, 31);
+            textQuest1.TabIndex = 7;
+            // 
+            // textEmail
+            // 
+            textEmail.Location = new Point(51, 239);
+            textEmail.Name = "textEmail";
+            textEmail.Size = new Size(434, 31);
+            textEmail.TabIndex = 6;
+            // 
+            // textCardId
+            // 
+            textCardId.Location = new Point(51, 155);
+            textCardId.Name = "textCardId";
+            textCardId.Size = new Size(434, 31);
+            textCardId.TabIndex = 5;
+            // 
+            // textFullname
+            // 
+            textFullname.Location = new Point(51, 71);
+            textFullname.Name = "textFullname";
+            textFullname.Size = new Size(434, 31);
+            textFullname.TabIndex = 4;
+            // 
+            // textQuest2
+            // 
+            textQuest2.Location = new Point(616, 323);
+            textQuest2.Name = "textQuest2";
+            textQuest2.Size = new Size(434, 31);
+            textQuest2.TabIndex = 11;
+            // 
+            // textReferenceId
+            // 
+            textReferenceId.Location = new Point(616, 239);
+            textReferenceId.Name = "textReferenceId";
+            textReferenceId.Size = new Size(434, 31);
+            textReferenceId.TabIndex = 10;
+            // 
+            // textPhone
+            // 
+            textPhone.Location = new Point(616, 155);
+            textPhone.Name = "textPhone";
+            textPhone.Size = new Size(434, 31);
+            textPhone.TabIndex = 9;
+            // 
+            // textUsername
+            // 
+            textUsername.Location = new Point(616, 71);
+            textUsername.Name = "textUsername";
+            textUsername.Size = new Size(434, 31);
+            textUsername.TabIndex = 8;
+            // 
+            // textPhoneAlt
+            // 
+            textPhoneAlt.Location = new Point(1165, 155);
+            textPhoneAlt.Name = "textPhoneAlt";
+            textPhoneAlt.Size = new Size(434, 31);
+            textPhoneAlt.TabIndex = 13;
+            // 
+            // textPassword
+            // 
+            textPassword.Location = new Point(1165, 71);
+            textPassword.Name = "textPassword";
+            textPassword.Size = new Size(434, 31);
+            textPassword.TabIndex = 12;
+            // 
+            // lblFullName
+            // 
+            lblFullName.AutoSize = true;
+            lblFullName.Location = new Point(51, 33);
+            lblFullName.Name = "lblFullName";
+            lblFullName.Size = new Size(91, 25);
+            lblFullName.TabIndex = 14;
+            lblFullName.Text = "Full Name";
+            // 
+            // lblCardID
+            // 
+            lblCardID.AutoSize = true;
+            lblCardID.Location = new Point(51, 115);
+            lblCardID.Name = "lblCardID";
+            lblCardID.Size = new Size(72, 25);
+            lblCardID.TabIndex = 15;
+            lblCardID.Text = "Card ID";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(51, 201);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(54, 25);
+            lblEmail.TabIndex = 16;
+            lblEmail.Text = "Email";
+            // 
+            // lblColor
+            // 
+            lblColor.AutoSize = true;
+            lblColor.Location = new Point(51, 284);
+            lblColor.Name = "lblColor";
+            lblColor.Size = new Size(198, 25);
+            lblColor.TabIndex = 17;
+            lblColor.Text = "What is Your Fav Color?";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(616, 33);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(91, 25);
+            lblUsername.TabIndex = 18;
+            lblUsername.Text = "Username";
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Location = new Point(616, 115);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new Size(62, 25);
+            lblPhone.TabIndex = 19;
+            lblPhone.Text = "Phone";
+            // 
+            // lblRefID
+            // 
+            lblRefID.AutoSize = true;
+            lblRefID.Location = new Point(616, 201);
+            lblRefID.Name = "lblRefID";
+            lblRefID.Size = new Size(111, 25);
+            lblRefID.TabIndex = 20;
+            lblRefID.Text = "Reference ID";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(816, 387);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 25);
+            label8.TabIndex = 21;
+            // 
+            // lblArtist
+            // 
+            lblArtist.AutoSize = true;
+            lblArtist.Location = new Point(616, 284);
+            lblArtist.Name = "lblArtist";
+            lblArtist.Size = new Size(254, 25);
+            lblArtist.TabIndex = 22;
+            lblArtist.Text = "What is Your Fav Artist/Singer?";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(1165, 33);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(87, 25);
+            lblPassword.TabIndex = 23;
+            lblPassword.Text = "Password";
+            // 
+            // lblPhoneAlt
+            // 
+            lblPhoneAlt.AutoSize = true;
+            lblPhoneAlt.Location = new Point(1165, 115);
+            lblPhoneAlt.Name = "lblPhoneAlt";
+            lblPhoneAlt.Size = new Size(89, 25);
+            lblPhoneAlt.TabIndex = 24;
+            lblPhoneAlt.Text = "Phone Alt";
+            // 
+            // buttonSummit
+            // 
+            buttonSummit.BackColor = Color.Lime;
+            buttonSummit.Location = new Point(58, 396);
+            buttonSummit.Name = "buttonSummit";
+            buttonSummit.Size = new Size(125, 34);
+            buttonSummit.TabIndex = 25;
+            buttonSummit.Text = "Submit";
+            buttonSummit.UseVisualStyleBackColor = false;
+            // 
+            // buttonBack
+            // 
+            buttonBack.BackColor = Color.Red;
+            buttonBack.Location = new Point(207, 396);
+            buttonBack.Name = "buttonBack";
+            buttonBack.Size = new Size(142, 34);
+            buttonBack.TabIndex = 26;
+            buttonBack.Text = "Back";
+            buttonBack.UseVisualStyleBackColor = false;
+            buttonBack.Click += buttonBack_Click;
+            // 
+            // labelError
+            // 
+            labelError.AutoSize = true;
+            labelError.ForeColor = Color.Red;
+            labelError.Location = new Point(461, 405);
+            labelError.Name = "labelError";
+            labelError.Size = new Size(24, 25);
+            labelError.TabIndex = 27;
+            labelError.Text = "...";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Location = new Point(1165, 201);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(77, 25);
+            lblAddress.TabIndex = 28;
+            lblAddress.Text = "Address";
+            // 
+            // textAddress
+            // 
+            textAddress.Location = new Point(1165, 239);
+            textAddress.Multiline = true;
+            textAddress.Name = "textAddress";
+            textAddress.Size = new Size(434, 115);
+            textAddress.TabIndex = 29;
+            // 
+            // RegistrationForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(1723, 798);
+            Controls.Add(textAddress);
+            Controls.Add(lblAddress);
+            Controls.Add(labelError);
+            Controls.Add(buttonBack);
+            Controls.Add(buttonSummit);
+            Controls.Add(lblPhoneAlt);
+            Controls.Add(lblPassword);
+            Controls.Add(lblArtist);
+            Controls.Add(label8);
+            Controls.Add(lblRefID);
+            Controls.Add(lblPhone);
+            Controls.Add(lblUsername);
+            Controls.Add(lblColor);
+            Controls.Add(lblEmail);
+            Controls.Add(lblCardID);
+            Controls.Add(lblFullName);
+            Controls.Add(textPhoneAlt);
+            Controls.Add(textPassword);
+            Controls.Add(textQuest2);
+            Controls.Add(textReferenceId);
+            Controls.Add(textPhone);
+            Controls.Add(textUsername);
+            Controls.Add(textQuest1);
+            Controls.Add(textEmail);
+            Controls.Add(textCardId);
+            Controls.Add(textFullname);
+            Name = "RegistrationForm";
+            Text = "RegistrationForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textQuest1;
+        private TextBox textEmail;
+        private TextBox textCardId;
+        private TextBox textFullname;
+        private TextBox textQuest2;
+        private TextBox textReferenceId;
+        private TextBox textPhone;
+        private TextBox textUsername;
+        private TextBox textPhoneAlt;
+        private TextBox textPassword;
+        private Label lblFullName;
+        private Label lblCardID;
+        private Label lblEmail;
+        private Label lblColor;
+        private Label lblUsername;
+        private Label lblPhone;
+        private Label lblRefID;
+        private Label label8;
+        private Label lblArtist;
+        private Label lblPassword;
+        private Label lblPhoneAlt;
+        private Button buttonSummit;
+        private Button buttonBack;
+        private Label labelError;
+        private Label lblAddress;
+        private TextBox textAddress;
+    }
+}
