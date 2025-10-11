6febf184a406b2f371394dfc99dd44f3adee9303
Miftahul Rizky <duokstayl376@gmail.com>
2025-10-11 12:58:44 +0700

    init: created folder by miftah

---
+++ Forms/DashboardPage.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Nusantara.Forms
+{
+    partial class DashboardPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            richBalance = new RichTextBox();
+            label1 = new Label();
+            lblBalance = new Label();
+            lblLoan = new Label();
+            label3 = new Label();
+            richLoan = new RichTextBox();
+            lblSaving = new Label();
+            label5 = new Label();
+            richSaving = new RichTextBox();
+            lblExchange = new Label();
+            label7 = new Label();
+            richExcgan = new RichTextBox();
+            lblAccrossTransactions = new Label();
+            label9 = new Label();
+            richAccrossTransaction = new RichTextBox();
+            lblInhouseTransaction = new Label();
+            label11 = new Label();
+            richInhouseTransactions = new RichTextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(39, 41);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1038, 130);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // richBalance
+            // 
+            richBalance.BackColor = SystemColors.ActiveCaption;
+            richBalance.Location = new Point(39, 206);
+            richBalance.Name = "richBalance";
+            richBalance.Size = new Size(334, 167);
+            richBalance.TabIndex = 1;
+            richBalance.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(56, 229);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Balance";
+            // 
+            // lblBalance
+            // 
+            lblBalance.AutoSize = true;
+            lblBalance.BackColor = SystemColors.ActiveCaption;
+            lblBalance.Location = new Point(318, 326);
+            lblBalance.Name = "lblBalance";
+            lblBalance.Size = new Size(22, 25);
+            lblBalance.TabIndex = 3;
+            lblBalance.Text = "0";
+            // 
+            // lblLoan
+            // 
+            lblLoan.AutoSize = true;
+            lblLoan.BackColor = SystemColors.ActiveCaption;
+            lblLoan.Location = new Point(670, 326);
+            lblLoan.Name = "lblLoan";
+            lblLoan.Size = new Size(22, 25);
+            lblLoan.TabIndex = 6;
+            lblLoan.Text = "0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaption;
+            label3.Location = new Point(408, 229);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Loan";
+            // 
+            // richLoan
+            // 
+            richLoan.BackColor = SystemColors.ActiveCaption;
+            richLoan.Location = new Point(391, 206);
+            richLoan.Name = "richLoan";
+            richLoan.Size = new Size(334, 167);
+            richLoan.TabIndex = 4;
+            richLoan.Text = "";
+            // 
+            // lblSaving
+            // 
+            lblSaving.AutoSize = true;
+            lblSaving.BackColor = SystemColors.ActiveCaption;
+            lblSaving.Location = new Point(1036, 326);
+            lblSaving.Name = "lblSaving";
+            lblSaving.Size = new Size(22, 25);
+            lblSaving.TabIndex = 9;
+            lblSaving.Text = "0";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ActiveCaption;
+            label5.Location = new Point(774, 229);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Saving";
+            // 
+            // richSaving
+            // 
+            richSaving.BackColor = SystemColors.ActiveCaption;
+            richSaving.Location = new Point(743, 206);
+            richSaving.Name = "richSaving";
+            richSaving.Size = new Size(334, 167);
+            richSaving.TabIndex = 7;
+            richSaving.Text = "";
+            // 
+            // lblExchange
+            // 
+            lblExchange.AutoSize = true;
+            lblExchange.BackColor = SystemColors.ActiveCaption;
+            lblExchange.Location = new Point(1036, 526);
+            lblExchange.Name = "lblExchange";
+            lblExchange.Size = new Size(22, 25);
+            lblExchange.TabIndex = 18;
+            lblExchange.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaption;
+            label7.Location = new Point(774, 429);
+            label7.Name = "label7";
+            label7.Size = new Size(86, 25);
+            label7.TabIndex = 17;
+            label7.Text = "Exchange";
+            // 
+            // richExcgan
+            // 
+            richExcgan.BackColor = SystemColors.ActiveCaption;
+            richExcgan.Location = new Point(743, 406);
+            richExcgan.Name = "richExcgan";
+            richExcgan.Size = new Size(334, 167);
+            richExcgan.TabIndex = 16;
+            richExcgan.Text = "";
+            // 
+            // lblAccrossTransactions
+            // 
+            lblAccrossTransactions.AutoSize = true;
+            lblAccrossTransactions.BackColor = SystemColors.ActiveCaption;
+            lblAccrossTransactions.Location = new Point(670, 526);
+            lblAccrossTransactions.Name = "lblAccrossTransactions";
+            lblAccrossTransactions.Size = new Size(22, 25);
+            lblAccrossTransactions.TabIndex = 15;
+            lblAccrossTransactions.Text = "0";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = SystemColors.ActiveCaption;
+            label9.Location = new Point(408, 429);
+            label9.Name = "label9";
+            label9.Size = new Size(174, 25);
+            label9.TabIndex = 14;
+            label9.Text = "Accross Transactions";
+            // 
+            // richAccrossTransaction
+            // 
+            richAccrossTransaction.BackColor = SystemColors.ActiveCaption;
+            richAccrossTransaction.Location = new Point(391, 406);
+            richAccrossTransaction.Name = "richAccrossTransaction";
+            richAccrossTransaction.Size = new Size(334, 167);
+            richAccrossTransaction.TabIndex = 13;
+            richAccrossTransaction.Text = "";
+            // 
+            // lblInhouseTransaction
+            // 
+            lblInhouseTransaction.AutoSize = true;
+            lblInhouseTransaction.BackColor = SystemColors.ActiveCaption;
+            lblInhouseTransaction.Location = new Point(318, 526);
+            lblInhouseTransaction.Name = "lblInhouseTransaction";
+            lblInhouseTransaction.Size = new Size(22, 25);
+            lblInhouseTransaction.TabIndex = 12;
+            lblInhouseTransaction.Text = "0";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = SystemColors.ActiveCaption;
+            label11.Location = new Point(56, 429);
+            label11.Name = "label11";
+            label11.Size = new Size(168, 25);
+            label11.TabIndex = 11;
+            label11.Text = "Inhouse Transaction";
+            // 
+            // richInhouseTransactions
+            // 
+            richInhouseTransactions.BackColor = SystemColors.ActiveCaption;
+            richInhouseTransactions.Location = new Point(39, 406);
+            richInhouseTransactions.Name = "richInhouseTransactions";
+            richInhouseTransactions.Size = new Size(334, 167);
+            richInhouseTransactions.TabIndex = 10;
+            richInhouseTransactions.Text = "";
+            // 
+            // DashboardPage
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(lblExchange);
+            Controls.Add(label7);
+            Controls.Add(richExcgan);
+            Controls.Add(lblAccrossTransactions);
+            Controls.Add(label9);
+            Controls.Add(richAccrossTransaction);
+            Controls.Add(lblInhouseTransaction);
+            Controls.Add(label11);
+            Controls.Add(richInhouseTransactions);
+            Controls.Add(lblSaving);
+            Controls.Add(label5);
+            Controls.Add(richSaving);
+            Controls.Add(lblLoan);
+            Controls.Add(label3);
+            Controls.Add(richLoan);
+            Controls.Add(lblBalance);
+            Controls.Add(label1);
+            Controls.Add(richBalance);
+            Controls.Add(richTextBox1);
+            Name = "DashboardPage";
+            Size = new Size(1121, 628);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private RichTextBox richBalance;
+        private Label label1;
+        private Label lblBalance;
+        private Label lblLoan;
+        private Label label3;
+        private RichTextBox richLoan;
+        private Label lblSaving;
+        private Label label5;
+        private RichTextBox richSaving;
+        private Label lblExchange;
+        private Label label7;
+        private RichTextBox richExcgan;
+        private Label lblAccrossTransactions;
+        private Label label9;
+        private RichTextBox richAccrossTransaction;
+        private Label lblInhouseTransaction;
+        private Label label11;
+        private RichTextBox richInhouseTransactions;
+    }
+}
---
+++ Forms/ForgotPasswordForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nusantara.Forms
+{
+    public partial class ForgotPasswordForm : Form
+    {
+        public ForgotPasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            using var db = new AppDbContext();
+            var auth = new AuthService(db);//EA1C1B
+            var password = await auth.ResetPasswordAsync(txtUsername.Text, txtQuest1.Text, txtQuest2.Text);
+            if (password == "")
+            {
+                MessageBox.Show("Invalid username or the answer", "Validation failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                txtPassword.Text = password;
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            loginForm loginForm = new LoginForm();
+            loginForm.ShowDialog();
+        }
+
+        private void ForgotPasswordForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
