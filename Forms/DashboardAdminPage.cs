387e74e91df202133c45860e4b58a73bccdf3b26
Miftahul Rizky <duokstayl376@gmail.com>
2025-10-18 12:11:12 +0700

    update dashboard(+admin) & config form, add ProductServices

--- Forms/DashboardAdminPage.cs
+++ Forms/DashboardAdminPage.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nusantara.Models;
 
 namespace Nusantara.Forms
 {
     public partial class DashboardAdminPage : UserControl
     {
-        public DashboardAdminPage()
+        private Member? loggedMember;
+        public DashboardAdminPage(Member? member)
         {
             InitializeComponent();
+            loggedMember = member;
+        }
+
+        private void DashboardAdminPage_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
