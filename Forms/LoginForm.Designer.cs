b2cdc359d73edb2359d914e0885e55977a6c1210
Rafreaks06 <mr.rafreaks@gmail.com>
2025-12-13 06:10:19 +0700

    adding route to login form

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -43,51 +43,57 @@
             // 
             lblLogin.AutoSize = true;
             lblLogin.Font = new Font("Segoe UI Historic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblLogin.Location = new Point(360, 57);
+            lblLogin.Location = new Point(252, 34);
+            lblLogin.Margin = new Padding(2, 0, 2, 0);
             lblLogin.Name = "lblLogin";
-            lblLogin.Size = new Size(131, 48);
+            lblLogin.Size = new Size(88, 32);
             lblLogin.TabIndex = 0;
             lblLogin.Text = "LOGIN";
             // 
             // lblUsername
             // 
             lblUsername.AutoSize = true;
-            lblUsername.Location = new Point(290, 137);
+            lblUsername.Location = new Point(203, 82);
+            lblUsername.Margin = new Padding(2, 0, 2, 0);
             lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(91, 25);
+            lblUsername.Size = new Size(60, 15);
             lblUsername.TabIndex = 1;
             lblUsername.Text = "Username";
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(290, 177);
+            txtUsername.Location = new Point(203, 106);
+            txtUsername.Margin = new Padding(2);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(254, 31);
+            txtUsername.Size = new Size(179, 23);
             txtUsername.TabIndex = 2;
             // 
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(290, 225);
+            lblPassword.Location = new Point(203, 135);
+            lblPassword.Margin = new Padding(2, 0, 2, 0);
             lblPassword.Name = "lblPassword";
-            lblPassword.Size = new Size(87, 25);
+            lblPassword.Size = new Size(57, 15);
             lblPassword.TabIndex = 3;
             lblPassword.Text = "Password";
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(290, 265);
+            txtPassword.Location = new Point(203, 159);
+            txtPassword.Margin = new Padding(2);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(254, 31);
+            txtPassword.Size = new Size(179, 23);
             txtPassword.TabIndex = 4;
             // 
             // btnSubmit
             // 
             btnSubmit.BackColor = SystemColors.Highlight;
             btnSubmit.ForeColor = SystemColors.ButtonHighlight;
-            btnSubmit.Location = new Point(288, 327);
+            btnSubmit.Location = new Point(202, 196);
+            btnSubmit.Margin = new Padding(2);
             btnSubmit.Name = "btnSubmit";
-            btnSubmit.Size = new Size(150, 34);
+            btnSubmit.Size = new Size(105, 20);
             btnSubmit.TabIndex = 5;
             btnSubmit.Text = "Submit";
             btnSubmit.UseVisualStyleBackColor = false;
@@ -97,38 +103,43 @@
             // 
             btnRegistrasion.BackColor = SystemColors.WindowFrame;
             btnRegistrasion.ForeColor = SystemColors.ButtonHighlight;
-            btnRegistrasion.Location = new Point(290, 381);
+            btnRegistrasion.Location = new Point(203, 229);
+            btnRegistrasion.Margin = new Padding(2);
             btnRegistrasion.Name = "btnRegistrasion";
-            btnRegistrasion.Size = new Size(148, 34);
+            btnRegistrasion.Size = new Size(104, 20);
             btnRegistrasion.TabIndex = 6;
             btnRegistrasion.Text = "Registrasion";
             btnRegistrasion.UseVisualStyleBackColor = false;
+            btnRegistrasion.Click += btnRegistrasion_Click;
             // 
             // linkForgetPassword
             // 
             linkForgetPassword.AutoSize = true;
-            linkForgetPassword.Location = new Point(514, 327);
+            linkForgetPassword.Location = new Point(360, 196);
+            linkForgetPassword.Margin = new Padding(2, 0, 2, 0);
             linkForgetPassword.Name = "linkForgetPassword";
-            linkForgetPassword.Size = new Size(144, 25);
+            linkForgetPassword.Size = new Size(94, 15);
             linkForgetPassword.TabIndex = 7;
             linkForgetPassword.TabStop = true;
             linkForgetPassword.Text = "Forget Password";
+            linkForgetPassword.LinkClicked += linkForgetPassword_LinkClicked;
             // 
             // lblValidasi
             // 
             lblValidasi.AutoSize = true;
-            lblValidasi.Location = new Point(290, 9);
+            lblValidasi.Location = new Point(203, 5);
+            lblValidasi.Margin = new Padding(2, 0, 2, 0);
             lblValidasi.Name = "lblValidasi";
-            lblValidasi.Size = new Size(24, 25);
+            lblValidasi.Size = new Size(16, 15);
             lblValidasi.TabIndex = 8;
             lblValidasi.Text = "...";
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.AppWorkspace;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 270);
             Controls.Add(lblValidasi);
             Controls.Add(linkForgetPassword);
             Controls.Add(btnRegistrasion);
@@ -139,6 +150,7 @@
             Controls.Add(lblUsername);
             Controls.Add(lblLogin);
             ForeColor = SystemColors.ActiveCaptionText;
+            Margin = new Padding(2);
             Name = "LoginForm";
             Text = "LoginForm";
             Load += LoginForm_Load;
