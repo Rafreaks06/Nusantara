5e1cc9d316eea17db0c6740112d321409a935eba
Mahasiswa <Mahasiswa@LABE-MULTIMEDIA>
2025-12-13 11:31:04 +0700

    Aku commit

--- Forms/ExchangePage.cs
+++ Forms/ExchangePage.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Nusantara.Data;
+using Nusantara.Models;
+using Nusantara.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,23 @@ namespace Nusantara.Forms
 {
     public partial class ExchangePage: UserControl
     {
-        public ExchangePage()
+        Member LoggedMember;
+        public ExchangePage(Member member)
         {
+            LoggedMember = member;
             InitializeComponent();
         }
+        private async void ExchangePage_Load(object sender, EventArgs e)
+        {
+            txtAmount.Text = "0";
+
+            AppDbContext db = new AppDbContext();
+            ConfigurationService configService = new ConfigurationService(db);
+            Configuration? config = await configService.GetConfig();
+
+            txtFee.Text = config != null ? config.transferAccessFee.ToString() : "0";
+            txtRate.Text = config != null ? config.exchangeRate.ToString() : "0";
+        }
+
     }
 }
