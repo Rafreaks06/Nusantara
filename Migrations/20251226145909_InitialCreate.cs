67682b304c11bab19fb02fc0b3c32e907cc4ee63
rafreaks06 <mr.rafreaks@gmail.com>
2025-12-26 22:00:11 +0700

    migration berhasil raffi

---
+++ Migrations/20251226145909_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Nusantara.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inhouses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    OriginId = table.Column<int>(type: "integer", nullable: false),
+                    DestinationId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Fee = table.Column<decimal>(type: "numeric", nullable: false),
+                    Remarks = table.Column<decimal>(type: "numeric", nullable: false),
+                    TransferDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TransferId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inhouses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inhouses_Members_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inhouses_Members_OriginId",
+                        column: x => x.OriginId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inhouses_DestinationId",
+                table: "Inhouses",
+                column: "DestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inhouses_OriginId",
+                table: "Inhouses",
+                column: "OriginId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inhouses");
+        }
+    }
+}
